2da1d5494268b9def9ebdfa8ae6561d241f287e9
Peter-John Welcome <pjwelcome@gmail.com>
2019-09-22 18:16:23 +0200

    Updating Dependencies
    Updated the gradle wrapper
    Updated the kotlin version to 1.3.50
    Updated the Android build tools
    Updated the compile version to 29 to support android 10
    Updated Appcompat to AndroidX
    Updated all the imports that used old Appcompat
    Updated retrofit gson converter version

--- app/src/main/java/com/multimeleon/pjapples/automldemo/MainActivity.kt
+++ app/src/main/java/com/multimeleon/pjapples/automldemo/MainActivity.kt
@@ -3,14 +3,13 @@ package com.multimeleon.pjapples.automldemo
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Bitmap.CompressFormat
-import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.provider.MediaStore
-import android.support.v7.app.AlertDialog
-import android.support.v7.app.AppCompatActivity
 import android.util.Base64
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -33,8 +32,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun choosePhotoFromGallary() {
-        val galleryIntent = Intent(Intent.ACTION_PICK,
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        val galleryIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
 
         startActivityForResult(galleryIntent, GALLERY)
     }
@@ -43,7 +44,8 @@ class MainActivity : AppCompatActivity() {
         val pictureDialog = AlertDialog.Builder(this)
         pictureDialog.setTitle("Select Action")
         val pictureDialogItems = arrayOf("Select photo from gallery", "Capture photo from camera")
-        pictureDialog.setItems(pictureDialogItems
+        pictureDialog.setItems(
+            pictureDialogItems
         ) { dialog, which ->
             when (which) {
                 0 -> choosePhotoFromGallary()
@@ -53,17 +55,14 @@ class MainActivity : AppCompatActivity() {
         pictureDialog.show()
     }
 
-    public override fun onActivityResult(requestCode:Int, resultCode:Int, data: Intent?) {
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == GALLERY)
-        {
-            if (data != null)
-            {
-                val contentURI = data?.data
-                try
-                {
+        if (requestCode == GALLERY) {
+            if (data != null) {
+                val contentURI = data.data
+                try {
                     val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
 
                     Toast.makeText(this@MainActivity, "Image Saved!", Toast.LENGTH_SHORT).show()
@@ -76,22 +75,29 @@ class MainActivity : AppCompatActivity() {
 
 
                     val requestBody = ModelRequestBody(PayloadRequest(ModelImage(imgString)))
-                    Network("https://automl.googleapis.com/v1beta1/",true).getRetrofitClient().create(Endpoint::class.java).classifyImage(requestBody).enqueue(object : Callback<PayloadResult> {
+                    Network("https://automl.googleapis.com/v1beta1/", true)
+                        .getRetrofitClient()
+                        .create(Endpoint::class.java)
+                        .classifyImage(requestBody)
+                        .enqueue(object : Callback<PayloadResult> {
 
-                        override fun onResponse(call: Call<PayloadResult>?, response: Response<PayloadResult>?) {
-                            if (response!!.isSuccessful) {
-                                Log.d("Hello", response?.body().toString())
-                                result_textview.text =  "${response?.body()?.items?.first()?.displayName} Score: ${(response?.body()?.items?.first()?.classification?.let { it.score * 100 })}"
+                            override fun onResponse(
+                                call: Call<PayloadResult>?,
+                                response: Response<PayloadResult>?
+                            ) {
+                                if (response!!.isSuccessful) {
+                                    Log.d("Hello", response.body().toString())
+                                    result_textview.text =
+                                        "${response.body()?.items?.first()?.displayName} Score: ${(response.body()?.items?.first()?.classification?.let { it.score * 100 })}"
+                                }
+                            }
+
+                            override fun onFailure(call: Call<PayloadResult>, t: Throwable) {
+                                print(t.message)
                             }
                         }
-
-                        override fun onFailure(call: Call<PayloadResult>, t: Throwable) {
-                            print(t!!.message)
-                        }
-                    }
-                    )
-                }
-                catch (e: IOException) {
+                        )
+                } catch (e: IOException) {
                     e.printStackTrace()
                     Toast.makeText(this@MainActivity, "Failed!", Toast.LENGTH_SHORT).show()
                 }
--- app/src/main/java/com/multimeleon/pjapples/automldemo/Network.kt
+++ app/src/main/java/com/multimeleon/pjapples/automldemo/Network.kt
@@ -1,7 +1,6 @@
 package com.multimeleon.pjapples.automldemo
 
-
-import android.support.annotation.VisibleForTesting
+import androidx.annotation.VisibleForTesting
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.annotations.SerializedName
@@ -46,7 +45,10 @@ class Network(private val baseUrl: String, enableLog: Boolean) {
 
         val headerAuthorizationInterceptor = Interceptor() {
             var request = it.request()
-            val headers = request.headers().newBuilder().add("Authorization","Bearer Enter Service Account Token here").build()
+            val headers = request.headers().newBuilder().add(
+                "Authorization",
+                "Bearer ya29.GqQBTQYqT73_XMREeuvPS_MOu2QG_SmqJk406Kq16m-0wksM_sw6ngvPncUoq75XA-PUwoTNA4YR3xcqcqgp2CGed3Bxj45IU4bbHU2X8t-XR2jxMjIV8GqEkmHiPd45uNr4EXRKz-VtV5tInoiStjCD_Xtuak1DVe8EjvOUHrFgdCc736Y_CHLkTQoJMVPPN-eAqcllOajFSAP9f-xuOz_LufwOorU"
+            ).build()
             request = request.newBuilder().headers(headers).build()
             return@Interceptor it.proceed(request)
         }
@@ -85,7 +87,7 @@ data class ModelImage (@field:SerializedName("imageBytes") val imageBytes : Stri
 internal interface Endpoint {
 
     @Headers("Content-Type: application/json")
-    @POST("projects/{ProjectName}/locations/us-central1/models/{AutoML_Model_ID}:predict")
+    @POST("projects/botanychecklistdatabase/locations/us-central1/models/ICN5073951862041042671:predict")
     fun classifyImage(@Body body: ModelRequestBody): Call<PayloadResult>
 }
 
